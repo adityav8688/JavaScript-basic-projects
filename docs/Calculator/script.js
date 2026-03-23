b0c62607f08127647f868a4e983e8ed96fe6b8a0
Aditya <adityavegulla@gmail.com>
2026-03-23 15:37:39 +0530

    feat: enhance calculator UI with new button styles and add backspace functionality

--- docs/Calculator/script.js
+++ docs/Calculator/script.js
@@ -2,6 +2,7 @@ const display = document.getElementById("display");
 const result = document.getElementById("result");
 const operators = ["+", "-", "*", "/", "."];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const errors = ["", "Error", "Undefined", "infinity"]
 
 function appendValue(value) {
   const lastChar = display.value.slice(-1);
@@ -9,23 +10,38 @@ function appendValue(value) {
   if (operators.includes(lastChar) && operators.includes(value)) {
     return;
   }
-  if ((value === ".") && (lastChar === ".")) {
-    return;
+  if (value === ".") {
+    const parts = display.value.split(/[\+\-\*\/]/);
+    const lastNumber = parts[parts.length - 1];
+
+    if (lastNumber.includes(".")) {
+      return;
+    }
   }
   display.value += value;
-}
+  result.value = eval(display.value);
+};
 
 function clearDisplay() {
   display.value = "";
-}
+  result.value = "";
+};
 
 function calculate() {
-  try {
+  display.value = result.value;
+  result.value = "";
+};
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+  if (display.value !== "") {
     result.value = eval(display.value);
-  } catch {
-    result.value = "Error";
+  } else if (errors.includes(result.value)) {
+    result.value = "";
+  } else {
+    result.value = "";
   }
-}
+};
 
 document.addEventListener("keydown", function (e) {
   const key = e.key;
@@ -35,7 +51,7 @@ document.addEventListener("keydown", function (e) {
   } else if (key === "Enter") {
     calculate();
   } else if (key === "Backspace") {
-    display.value = display.value.slice(0, -1);
+    backspace();
   } else if (key === "Escape") {
     clearDisplay();
   }
