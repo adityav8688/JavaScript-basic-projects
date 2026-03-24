7441ea75ae1a4ec76326f8ad6356b4769de59d30
Aditya <adityavegulla@gmail.com>
2026-03-24 18:51:38 +0530

    feat: enhance Live Search Filter with navigation, item addition, and improved rendering

--- docs/Live-search-filter/script.js
+++ docs/Live-search-filter/script.js
@@ -2,22 +2,24 @@ let searchBar = document.getElementById("searchBar");
 let itemInput = document.getElementById("itemInput");
 let addBtn = document.getElementById("addBtn");
 let itemsList = document.getElementById("itemsList");
+let itemNames = document.querySelectorAll("li");
 
 let items = JSON.parse(localStorage.getItem("items")) || [];
-console.log(typeof items);
 
-itemInput.addEventListener("keypress", function(e){
-    if(e.key === "Enter"){
+
+
+itemInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
         addBtn.click();
     }
 });
 
 renderItems();
 
-addBtn.addEventListener("click", function(){
+addBtn.addEventListener("click", function () {
     let itemVal = itemInput.value.trim();
 
-    if(itemVal === "") return;
+    if (itemVal === "") return;
 
     items.push(itemVal);
     localStorage.setItem("items", JSON.stringify(items));
@@ -26,15 +28,43 @@ addBtn.addEventListener("click", function(){
     itemInput.value = "";
 });
 
-function renderItems(){
-    console.log("renderItems");
+searchBar.addEventListener("keyup", function () {
+    renderItems(searchBar.value);
+});
+
+function renderItems(filter = "") {
     itemsList.innerHTML = "";
 
-    items.forEach(function(item, index) {
-        let li = document.createElement("li");
-        li.classList.add("item"+index);
-        li.style.listStyle = "none";
-        li.innerText = item;
-        itemsList.appendChild(li);
-    });
+    items.filter(item =>
+        item.toLowerCase().split(" ")
+            .some(word => word.startsWith(filter.toLowerCase()))
+    )
+        .forEach((item, index) => {
+            let li = document.createElement("li");
+            li.classList.add("item" + index);
+
+            let span = document.createElement("span");
+            span.innerText = item;
+            span.classList.add("itemInfo");
+            
+
+            let delBtn = document.createElement("button");
+            delBtn.classList.add("delBtn");
+            delBtn.innerHTML = `<span class="material-symbols-outlined">delete</span>`
+
+            delBtn.addEventListener("click", function () {
+                deleteItem(index);
+            });
+
+            li.appendChild(span);
+            li.appendChild(delBtn);
+            itemsList.appendChild(li);
+        });
+};
+
+function deleteItem(index) {
+    items.splice(index, 1);
+    localStorage.setItem("items", JSON.stringify(items));
+
+    renderItems();
 };
